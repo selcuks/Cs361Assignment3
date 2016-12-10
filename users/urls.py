088b3e606da7b0c3718050d4d07c1801ba484420
selcuksivrioglu <selcuksivrioglu@std.sehir.edu.tr>
2016-12-10 16:09:09 +0200

    Initial commit

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+
+from .views import signup
+
+
+
+urlpatterns = [
+    url(r'^users/register/$', signup),
+    url(r'', include("django.contrib.auth.urls")),
+
+
+]
